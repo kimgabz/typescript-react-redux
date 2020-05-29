a6458117b8174cda1f4102130dd49eaaffd31b2b
kim <khemgabz@gmail.com>
2020-05-29 10:50:04 +0800

    implement redux to component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
+import { connect } from 'react-redux';
 
-export class App extends React.Component {
+interface AppProps {
+    todos: Todo[];
+    fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
 
     render() {
         return <div>Welcome to Summoner's Rift</div>;
     }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+    return { todos };
+}
+
+export const App = connect(
+    mapStateToProps,
+    { fetchTodos }
+)(_App);

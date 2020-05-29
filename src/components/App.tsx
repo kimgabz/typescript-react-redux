cd8d24f7d9d22e788e472a8c330e921ca05d3652
kim <khemgabz@gmail.com>
2020-05-29 15:19:15 +0800

    fetch and delete todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,61 @@
 import React from 'react';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 import { connect } from 'react-redux';
 
 interface AppProps {
     todos: Todo[];
-    fetchTodos(): any;
+    fetchTodos: Function;
+    deleteTodo: typeof deleteTodo;
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+    fetching: boolean;
+}
+
+class _App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = { fetching: false }
+    }
+
+    componentDidUpdate(prevProps: AppProps): void {
+        if (!prevProps.todos.length && this.props.todos.length) {
+            this.setState({ fetching: false });
+        }
+    }
+
+    onButtonCLick = (): void => {
+        this.props.fetchTodos();
+        this.setState({ fetching: true})
+    }
+
+    onTodoCLick = (id: number): void => {
+        this.props.deleteTodo(id);
+    }
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+            return (
+                <div onClick={() => this.onTodoCLick(todo.id)} 
+                     key={ todo.id }>
+                         { todo.title }
+                    </div>
+            );
+        });
+    }
 
     render() {
-        return <div>Welcome to Summoner's Rift</div>;
+        return (
+            <div>
+                <div>Welcome to Summoner's Rift</div>
+                <button onClick={this.onButtonCLick}>Fetch</button>
+                { this.state.fetching ? 'LOADING' : null }
+                { this.renderList() }
+            </div>
+        );
     }
 }
 
@@ -21,5 +65,5 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
 
 export const App = connect(
     mapStateToProps,
-    { fetchTodos }
+    { fetchTodos, deleteTodo }
 )(_App);
